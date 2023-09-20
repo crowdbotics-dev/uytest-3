b0bb9b151d70437d7b76700c179259cadd1a2dbd
Crowdbotics <engineering@crowdbotics.com>
2023-09-20 18:01:12 +0000

    Update store.

--- store/new/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-import { NEW_43_USERNAME, NEW_43_PASSWORD } from "react-native-dotenv"
-const new = axios.create({
-  baseURL: "https://devstringx.com",
-  auth: { username: NEW_43_USERNAME, password: NEW_43_PASSWORD },
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
-export const apiService = {}
